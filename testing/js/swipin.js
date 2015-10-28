dd16feb306100b8fb30c15413fa06642ad7eaafb
zurbchris <chris@zurb.com>
2015-10-28 15:47:58 -0500

    adds motion-ui animations for swiped cards

--- testing/js/swipin.js
+++ testing/js/swipin.js
@@ -2,7 +2,6 @@
   'use strict';
 
   function Swipin(element, options){
-    console.log('wtf mate');
     this.$element = element;
     this.options = $.extend({}, Swipin.defaults, this.$element.data(), options || {});
 
@@ -18,29 +17,66 @@
 
   Swipin.prototype._init = function(){
     this.$cards = this.$element.find('.card');
-
+    this.$cards.each(function(){
+      var $card = $(this);
+      // $card.addTouch();
+    });
+    if(!Foundation.MediaQuery.atLeast(this.options.unstackOn)){
+      this._stack();
+    }
     this._events();
   };
 
   Swipin.prototype._events = function(){
     var _this = this;
-    $(window).on('changed.zf.mediaquery', function(e, newSize, oldSize){
-      console.log(newSize);
-      _this._handleMQChange(newSize, oldSize);
-    });
+    $(window).off('.zf.mediaquery')
+             .on('changed.zf.mediaquery', function(e, newSize, oldSize){
+               _this._handleMQChange(newSize, oldSize);
+             });
 
+    if(this.stacked){
+      this._addSwipe();
+    }
+
+    this.$cards.off('click.zf.swipin tap.zf.swipin')
+               .on('click.zf.swipin tap.zf.swipin', function(e){
+                 console.log('mouseup', e);
+               });
   };
+  Swipin.prototype._addSwipe = function(){
+    var _this = this;
+    this.$cards.on('swiperight.zf.swipin', function(e){
+       _this._swipe($(this), 'Right');
+      }).on('swipeleft.zf.swipin', function(e){
+       _this._swipe($(this), 'Left');
+      });
+  };
+  Swipin.prototype._pauseSwipe = function(){
+    this.$cards.off('swipeleft.zf.swipin swiperight.zf.swipin');
+  }
+
+  Swipin.prototype._swipe = function($card, dir){
+    var trigger = (dir === 'Right' ? 'yesplease' : 'nothanks') + '.zf.card';
+
+    Foundation.Motion.animateOut($card, 'slideOut' + dir, function(){
+      console.log('animation done', $card);
+      $card.trigger(trigger, [$card]).remove();
+    });
+  };
+
   Swipin.prototype._handleMQChange = function(newSize, oldSize){
     var _this = this;
-
     if(Foundation.MediaQuery.atLeast(this.options.unstackOn)){
       if(this.stacked){
+        this._pauseSwipe();
         this._unstack();
       }
     }else{
       this._stack();
+      this._addSwipe();
     }
   };
+
   Swipin.prototype._stack = function(){
     this.$cards.addClass('is-stacked');
     this.stacked = true;
