3ca76e6b4b62b7afc5c4e26471ef11f27ca4fc1a
Brandon Arnold <brandon@zurb.com>
2015-11-10 14:24:13 -0800

    add analytics

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -15,3 +15,19 @@ $(function() {
 $(function() {
   $('[data-docs-version]').text('v' + Foundation.version);
 });
+
+
+// Analytics 
+var _gaq = _gaq || [];
+_gaq.push(
+  ['_setAccount', 'UA-2195009-2'],
+  ['_trackPageview'],
+  ['b._setAccount', 'UA-2195009-27'],
+  ['b._trackPageview']
+);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
