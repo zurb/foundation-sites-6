09d445c5e241f66a42261cd83bdc7d85fe67472b
zurbchris <chris@zurb.com>
2015-11-16 10:06:15 -0800

    improves nest.burn function to remove more js-applied values to navigation menus, removes tabindex attr from nest.feather

--- js/foundation.util.nest.js
+++ js/foundation.util.nest.js
@@ -3,7 +3,7 @@
   Foundation.Nest = {
     Feather: function(menu, type){
       menu.attr('role', 'menubar');
-      var items = menu.find('li').attr({'role': 'menuitem', 'tabindex': 0}),
+      var items = menu.find('li').attr({'role': 'menuitem'/*, 'tabindex': 0*/}),
           type = type || 'zf',
           subMenuClass = 'is-' + type + '-submenu',
           subItemClass = subMenuClass + '-item';
@@ -27,18 +27,24 @@
           subItemClass = subMenuClass + '-item';
 
       menu.find('.is-active').removeClass('is-active');
+      menu.find('.' + subMenuClass + ', .' + subItemClass + ', .has-submenu, .is-submenu-item, .submenu, [data-submenu]')
+          .removeClass(subMenuClass + ' ' + subItemClass + 'has-submenu is-submenu-item submenu')
+          .removeAttr('data-submenu').css('display', '');
 
-      items.each(function(){
-        var $item = $(this),
-            $sub = $item.children('ul');
-        if($item.parent('[data-submenu]').length){
-          $item.removeClass('is-submenu-item ' + subItemClass);
-        }
-        if($sub.length){
-          $item.removeClass('has-submenu');
-          $sub.removeClass('submenu ' + subMenuClass).removeAttr('data-submenu');
-        }
-      });
+      console.log(      menu.find('.' + subMenuClass + ', .' + subItemClass + ', .has-submenu, .is-submenu-item, .submenu, [data-submenu]')
+                .removeClass(subMenuClass + ' ' + subItemClass + ' has-submenu is-submenu-item submenu')
+                .removeAttr('data-submenu'));
+      // items.each(function(){
+      //   var $item = $(this),
+      //       $sub = $item.children('ul');
+      //   if($item.parent('[data-submenu]').length){
+      //     $item.removeClass('is-submenu-item ' + subItemClass);
+      //   }
+      //   if($sub.length){
+      //     $item.removeClass('has-submenu');
+      //     $sub.removeClass('submenu ' + subMenuClass).removeAttr('data-submenu');
+      //   }
+      // });
     }
   };
 }(jQuery, window.Foundation);
